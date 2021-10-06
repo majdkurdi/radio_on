af532aa67fe8a7ee9282951a9d70ca11e658487e
unknown <majdkurdi1998@gmail.com>
2021-10-07 00:06:38 +0300

    screenshots

--- lib/providers/songs_provider.dart
+++ lib/providers/songs_provider.dart
@@ -1,7 +1,6 @@
 import 'dart:io';
 
 import 'package:flutter/cupertino.dart';
-import 'package:flutter_audio_query/flutter_audio_query.dart';
 import 'package:permission_handler/permission_handler.dart';
 import '../modals/song.dart';
 
